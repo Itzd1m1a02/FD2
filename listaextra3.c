d7421826e8be24f6b2cacbb68a3b19c9c170b68f
Dimitri pereira maia <93716158+Itzd1m1a02@users.noreply.github.com>
2023-04-14 15:53:53 -0300

    triangulo

---
+++ listaextra3.c
@@ -0,0 +1,44 @@
+/**
+ * @file listaextra2.c
+ * @author dimitri (dimitrimaia39@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-04-12
+ * 
+ * @copyright Copyright (c) 2023
+ * Elabore uma função que receba como parâmetro um valor inteiro n e gere como saída um triângulo lateral formado por asteriscos conforme o exemplo a seguir, em que usamos n = 4:
+*
+**
+***
+****
+***
+**
+*
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void triangulo(int x){
+    for (int i = 1; i <= x; i++){
+        for (int j = 1; j <= i; j++){
+            printf("*");
+        }//for
+        printf("\n");
+    }//for
+    for (int i = x; i >= 1; i--){
+        for (int j = i; j >= 1; j--){
+            printf("*");
+        }//for
+        printf("\n");
+    }//for
+}//função
+
+int main(){
+    int num;
+    printf("Digite um numero: ");
+    scanf("%d", &num);
+
+    triangulo(num);
+}//main
